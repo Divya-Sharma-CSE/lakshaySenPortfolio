6050caf9a9fc8dabfe65c0d97cacd468b26c9feb
Divya-Sharma-CSE <divyash0242@gmail.com>
2025-12-19 04:45:17 +0530

    hero completed

--- Untitled-1.js
+++ Untitled-1.js
@@ -2,3 +2,22 @@ window.addEventListener("scroll", () => {
   const nav = document.querySelector(".navBar");
   nav.classList.toggle("scrolled", window.scrollY > window.innerHeight - 80);
 });
+
+// MENU EXPAND
+const hamburger = document.querySelector(".sections");
+const overlay = document.getElementById("menuOverlay");
+const body = document.body;
+
+hamburger.addEventListener("click", () => {
+  body.classList.add("menu-open");
+});
+
+overlay.addEventListener("click", () => {
+  body.classList.remove("menu-open");
+});
+
+document.addEventListener("keydown", (e) => {
+  if(e.key === "Escape"){
+    body.classList.remove("menu-open");
+  }
+});
--- script.js
+++ script.js
@@ -1,4 +1,49 @@
-window.addEventListener("scroll", () => {
-  const nav = document.querySelector(".navbar");
-  nav.classList.toggle("scrolled", window.scrollY > window.innerHeight - 60);
+//SHUTTLE SCROLLING EFFECT
+const nav = document.querySelector(".shuttle");
+
+nav.addEventListener("click", () => {
+    const shuttle = document.querySelector(".heroShuttle");
+    const bio = document.querySelector(".bio");
+    bio.scrollIntoView({ behavior: "smooth", block: "start" });
+
+    shuttle.style.visibility = "visible";
+
 });
+
+/* 
+//MENU EXPAND    
+const menu = document.querySelector(".selections");
+const hero = document.querySelector(".heroPage");
+
+menu.addEventListener("click", () => {
+    hero.style.backgroundColor= rgba(0, 0, 0, 0.5);
+    
+}); */
+
+
+//MENU EXPAND
+const arrow = document.querySelector(".sections");
+const overlay = document.getElementById("menuOverlay");
+const closeBtn = document.querySelector(".closeBtn");
+const body = document.body;
+
+arrow.addEventListener("click", () => {
+    body.classList.add("menu-open");
+});
+
+overlay.addEventListener("click", () => {
+    body.classList.remove("menu-open");
+});
+
+closeBtn.addEventListener("click", () => {
+    body.classList.remove("menu-open");
+});
+
+document.addEventListener("keydown", (e) => {
+  if(e.key === "Escape"){
+    body.classList.remove("menu-open");
+  }
+});
+
+
+
