d111137b6e52c743d0387540301bfd25b05830f3
Divya-Sharma-CSE <divyash0242@gmail.com>
2025-12-28 19:37:39 +0530

    gallery completed

--- Untitled-1.js
+++ Untitled-1.js
@@ -1,7 +1,35 @@
-const track = document.querySelector(".track");
+const images = document.querySelectorAll('.galleryImg');
+const lightbox = document.getElementById('lightbox');
+const lightboxImg = document.getElementById('lightboxImg');
+const lightboxTitle = document.getElementById('lightboxTitle');
+const lightboxDesc = document.getElementById('lightboxDesc');
+const closeBtn = document.querySelector('.lightboxClose');
 
-document.addEventListener("mousemove", (e) => {
-  const speed = e.clientX / window.innerWidth;
-  track.style.animationDuration = `${30 - speed * 20}s`;
+// OPEN LIGHTBOX
+images.forEach(img => {
+  img.addEventListener('click', () => {
+    lightbox.classList.add('active');
+    lightboxImg.src = img.src;
+    lightboxTitle.textContent = img.dataset.title;
+    lightboxDesc.textContent = img.dataset.desc;
+  });
 });
 
+// CLOSE BUTTON
+closeBtn.addEventListener('click', () => {
+  lightbox.classList.remove('active');
+});
+
+// CLICK OUTSIDE IMAGE TO CLOSE
+lightbox.addEventListener('click', (e) => {
+  if (e.target === lightbox) {
+    lightbox.classList.remove('active');
+  }
+});
+
+// ESC KEY CLOSE
+document.addEventListener('keydown', (e) => {
+  if (e.key === 'Escape') {
+    lightbox.classList.remove('active');
+  }
+});
--- script.js
+++ script.js
@@ -162,3 +162,39 @@ function sortTable(colIndex) {
   rows.forEach(row => tbody.appendChild(row))};
 
 
+//LightBox
+const images = document.querySelectorAll('.galleryImg');
+const lightbox = document.getElementById('lightbox');
+const lightboxImg = document.getElementById('lightboxImg');
+const lightboxTitle = document.getElementById('lightboxTitle');
+const lightboxDesc = document.getElementById('lightboxDesc');
+const closeB = document.querySelector('.lightboxClose');
+
+// OPEN LIGHTBOX
+images.forEach(img => {
+  img.addEventListener('click', () => {
+    lightbox.classList.add('active');
+    lightboxImg.src = img.src;
+    lightboxTitle.textContent = img.dataset.title;
+    lightboxDesc.textContent = img.dataset.desc;
+  });
+});
+
+// CLOSE BUTTON
+closeB.addEventListener('click', () => {
+  lightbox.classList.remove('active');
+});
+
+// CLICK OUTSIDE IMAGE TO CLOSE
+lightbox.addEventListener('click', (e) => {
+  if (e.target === lightbox) {
+    lightbox.classList.remove('active');
+  }
+});
+
+// ESC KEY CLOSE
+document.addEventListener('keydown', (e) => {
+  if (e.key === 'Escape') {
+    lightbox.classList.remove('active');
+  }
+});
